4a6e9d774975da970a3dc806770479d9bb6c9f72
JaehongDev <println@kakao.com>
2023-08-05 19:23:57 +0900

    Initial commit

---
+++ src/main/java/com/example/crawlerbatchtest/CrawlerBatchTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.crawlerbatchtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerBatchTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlerBatchTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crawlerbatchtest/CrawlerBatchTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crawlerbatchtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlerBatchTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
